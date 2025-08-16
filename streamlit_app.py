90e928ccdf7c6c2eb642b2f11ef444653dfc67b1
samanthaliu525 <samantha.liu525@gmail.com>
2025-08-16 12:57:17 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ import plotly.express as px
 df = px.data.iris()
 pxfig = px.scatter(df, x="sepal_width",
 y="sepal_length", color="species",
-size=’petal_length’,
+size= 'petal_length’,
 hover_data=[’petal_width’])
 st.plotly_chart(pxfig, use_container_width=True)
 
