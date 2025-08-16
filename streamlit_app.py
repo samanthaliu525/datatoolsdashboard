2cb0f189b909e161e5c21d1c5cb097862ed26f6f
samanthaliu525 <samantha.liu525@gmail.com>
2025-08-16 10:01:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ import numpy as np
 
 st.title("Data Tools Dashboard")
 st.write("Explore the relationships between CO2 emissions and other variables")
+
+CO2_long = pd.Dataframe("CO2_long")
+
+st.dataframe(CO2_long)
