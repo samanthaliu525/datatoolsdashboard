479339b0fcdad6b2b78bb822f3d52b6ee13c2cb8
samanthaliu525 <samantha.liu525@gmail.com>
2025-08-16 13:33:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,3 +58,28 @@ fig.update_layout(
 
 # Display the animated Plotly chart in Streamlit
 st.plotly_chart(fig, use_container_width=True)
+
+CO2_temp_US_scaled_url="https://raw.githubusercontent.com/samanthaliu525/datatoolsdashboard/d3f902e6facd88596ae1367741663ba914119887/CO2_temp_US_scaled.png"
+CO2_temp_US_facet_url= "https://raw.githubusercontent.com/samanthaliu525/datatoolsdashboard/d3f902e6facd88596ae1367741663ba9119887/CO2_temp_US_facet.png"
+CO2_temp_China_scaled_url = "https://raw.githubusercontent.com/samanthaliu525/datatoolsdashboard/37b578aaecd79f967b66478f6a56ffe878e217ee/CO2_temp_China_scaled.png"
+
+col1, col2 = st.columns(2)
+with col1:
+    show_us = st.button("United States")
+with col2:
+    show_china = st.button("China")
+
+# Use a state variable to track which button was clicked last
+if 'last_clicked' not in st.session_state:
+    st.session_state.last_clicked = 'all'
+
+if show_us:
+    st.session_state.last_clicked = 'us'
+elif show_china:
+    st.session_state.last_clicked = 'china'
+
+# Display the correct image based on the last clicked button
+if st.session_state.last_clicked == 'us':
+    st.image(CO2_temp_US_scaled_url, caption="United States")
+elif st.session_state.last_clicked == 'china':
+    st.image(CO2_temp_China_scaled_url, caption="China")
