aed643ef280543befcc93e4a00c66797dda4da88
samanthaliu525 <samantha.liu525@gmail.com>
2025-08-16 12:28:34 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
+import plotly.express as px
+
 
 st.title("Data Tools Dashboard")
 st.write("Explore the relationships between and progression of CO₂ Emissions, temperature, GDP per capita, energy use, and number of natural disasters")
