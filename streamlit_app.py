9ae86868d835bb231a462ee21e651ea3d6b4eb9a
samanthaliu525 <samantha.liu525@gmail.com>
2025-08-16 12:10:09 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,13 @@
-# app.py
-
+# import libraries
 import streamlit as st
 import pandas as pd
 import numpy as np
+import altair as alt
+
+st.title("Data Tools Dashboard")
+st.write("Explore the relationships between and progression of CO₂ Emissions, temperature, GDP per capita, energy use, and number of natural disasters")
 
 st.subheader("Static World CO₂ Emissions Line Chart")
-
-
 CO2_plot_url = 'https://raw.githubusercontent.com/samanthaliu525/datatoolsdashboard/85d69e7e6c2771f7ea331a477029062ec754d08d/CO2_world.png'
 st.image(CO2_plot_url, caption='World CO₂ Emissions per Year')
 
@@ -32,3 +33,21 @@ if 'Year' in df.columns:
     st.bar_chart(filtered_df_bar, x='Country', y='Emissions')
 else:
     st.info("Data does not contain a 'Year' column for this example.")
+
+
+# Filter the DataFrame based on the selected year range
+CO2_visuals_url = "https://raw.githubusercontent.com/samanthaliu525/datatoolsdashboard/refs/heads/main/CO2_visuals.csv"
+CO2_visuals = pd.read_csv(CO2_visuals_url)
+
+# Create the Altair line chart
+fig_co2 = alt.Chart(CO2_visuals).mark_line().encode(
+    x='Year',
+    y='Emissions',
+    color=alt.Color('Country', legend=alt.Legend(title="Country")),
+    tooltip=['Country', 'Year', 'Emissions']
+).properties(
+    title='Country CO₂ Emissions per Year'
+)
+
+# Display the Altair chart in Streamlit
+st.altair_chart(fig_co2, use_container_width=True)
