5a4dbb283f9a1c5bb0b4500685cfff292cdb43d9
samanthaliu525 <samantha.liu525@gmail.com>
2025-08-16 10:13:22 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import numpy as np
 st.title("Data Tools Dashboard")
 st.write("Explore the relationships between CO2 emissions and other variables")
 
-CO2_clean_url= CO2_world_clean.csv
+CO2_clean_url= https://github.com/samanthaliu525/datatoolsdashboard/blob/24e1035eacaff777683cb67e24589a71d24a2c43/CO2_world_clean.csv
 
 st.dataframe(CO2_clean_url)
 
