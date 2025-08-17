a335f246f585e57a6b1980bc8d873abb32331395
samanthaliu525 <samantha.liu525@gmail.com>
2025-08-17 07:19:37 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,10 +10,16 @@ import altair as alt
 st.title("Data Tools Dashboard")
 st.write("Explore the relationships between and progression of CO₂ Emissions, temperature, GDP per capita, energy use, and number of natural disasters")
 
+if st.button('Say Hello'):
+    st.write('Hello')
+    
 st.subheader("Static World CO₂ Emissions Line Chart")
 CO2_plot_url = 'https://raw.githubusercontent.com/samanthaliu525/datatoolsdashboard/85d69e7e6c2771f7ea331a477029062ec754d08d/CO2_world.png'
 st.image(CO2_plot_url, caption='World CO₂ Emissions per Year')
 
+st.subheader("US CO₂ Emissions and Temperature")
+CO2_plot_url = 'https://raw.githubusercontent.com/samanthaliu525/datatoolsdashboard/e8a9875fd403e4bf1cd400dc73b59b103884e26d/CO2_temp_US_scaled.png'
+st.image(CO2_plot_url, caption='World CO₂ Emissions per Year')
 
 top_emittors_url = "https://raw.githubusercontent.com/samanthaliu525/datatoolsdashboard/main/top_10.csv"
 top_emittors = pd.read_csv(top_emittors_url)
